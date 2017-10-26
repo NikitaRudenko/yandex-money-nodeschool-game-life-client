9577d8b20c7cd6f7eef702eb9de639ca168ca0b7
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-10-26 23:47:26 +0300

    Add init method

--- main/index.js
+++ main/index.js
@@ -1,22 +1,44 @@
-'use strict';
+/**
+ * Connect to server and send token
+ * @param {String} token
+ * @returns {WebSocket}
+ */
+const connect = (token) => {
+    return new WebSocket(`ws://ws.rudenko.tech/life/api?token=${token}`);
+};
 
-//
-// YOUR CODE GOES HERE...
-//
-// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-// ░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░
-// ░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░░
-// ░░░░░░░░█░░▄░░░░▄░░░░░░░░░░░░░░█░░░░░░░░
-// ░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░░
-// ░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░░
-// ░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░░
-// ░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░░
-// ░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░░
-// ░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░░
-// ░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░░
-// ░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░░░
-// ░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░░
-// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-//
-// Nyan cat lies here...
-//
+/**
+ * Add events listeners
+ * @param {WebSocket} socket
+ */
+const setupListeners = (socket) => {
+    socket.addEventListener('open', () => {
+        console.log('Connected to server!')
+    });
+
+    socket.addEventListener('message', (event) => {
+        console.log('event:', event);
+    });
+
+    socket.addEventListener('error', (event) => {
+        console.log('Error:', event);
+    });
+
+    socket.addEventListener('Close', () => {
+        console.log('Connection closed!');
+    });
+};
+
+/**
+ * Init app
+ * @param {String} token
+ */
+const init = (token) => {
+    console.log('token:', token);
+
+    const socket = connect(token);
+
+    setupListeners(socket);
+};
+
+App.onToken = init;
