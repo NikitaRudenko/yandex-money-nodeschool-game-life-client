86c6a8889901acd1750a1a0b7108e8ae0b748e81
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-10-27 09:43:05 +0300

    Add Network class

--- main/index.js
+++ main/index.js
@@ -1,44 +1,85 @@
 /**
- * Connect to server and send token
- * @param {String} token
- * @returns {WebSocket}
+ * Creates a new Network class
+ * @class
  */
-const connect = (token) => {
-    return new WebSocket(`ws://ws.rudenko.tech/life/api?token=${token}`);
-};
+class Network {
+    /**
+     * Constructor
+     */
+    constructor(token) {
+        this.token = token;
+        this.connect = this.connect.bind(this);
+        this.setupListeners = this.setupListeners.bind(this);
+        this.onOpen = this.onOpen.bind(this);
+        this.onClose = this.onClose.bind(this);
+        this.onMessage = this.onMessage.bind(this);
+        this.onError = this.onError.bind(this);
+        this.init = this.init.bind(this);
 
-/**
- * Add events listeners
- * @param {WebSocket} socket
- */
-const setupListeners = (socket) => {
-    socket.addEventListener('open', () => {
+        this.init();
+    }
+
+    /**
+     * Connect to server and send token
+     */
+    connect() {
+        this.socket = new WebSocket(`ws://ws.rudenko.tech/life/api?token=${this.token}`);
+    }
+
+    /**
+     * Add events listeners
+     */
+    setupListeners() {
+        this.socket.addEventListener('open', this.onOpen);
+        this.socket.addEventListener('close', this.onClose);
+        this.socket.addEventListener('message', this.onMessage);
+        this.socket.addEventListener('error', this.onError);
+    }
+
+    /**
+     * Handle open event
+     */
+    onOpen() {
         console.log('Connected to server!')
-    });
+    }
 
-    socket.addEventListener('message', (event) => {
+    /**
+     * Handle close event
+     * @param {Object} event
+     */
+    onClose(event) {
+        if (event.wasClean) {
+            console.log('Connection closed!');
+        } else {
+            console.log('Connection closed with error! Trying to reconnect!');
+
+            this.init();
+        }
+    }
+
+    /**
+     * Handle message event
+     * @param {Object} event
+     */
+    onMessage(event) {
         console.log('event:', event);
-    });
+    }
 
-    socket.addEventListener('error', (event) => {
-        console.log('Error:', event);
-    });
+    /**
+     * Handle error event
+     * @param {Object} error
+     */
+    onError(error) {
+        console.log('Error:', error.message);
+    }
 
-    socket.addEventListener('Close', () => {
-        console.log('Connection closed!');
-    });
-};
+    /**
+     * Init app
+     */
+    init() {
+        this.connect();
+        this.setupListeners();
+    }
+}
 
-/**
- * Init app
- * @param {String} token
- */
-const init = (token) => {
-    console.log('token:', token);
-
-    const socket = connect(token);
-
-    setupListeners(socket);
-};
-
-App.onToken = init;
+App.onToken = (token) => (new Network(token));
