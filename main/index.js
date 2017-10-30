f74e26087ebfcfae0daf8a3f353c8cc6ade1fdb7
Andrei Kiselev <aw.v.kiselev@gmail.com>
2017-10-30 09:43:26 +0300

    add sockets

--- main/index.js
+++ main/index.js
@@ -20,3 +20,73 @@
 //
 // Nyan cat lies here...
 //
+
+let socket = {};
+let game = {};
+
+App.onToken = (token) => {
+    socket = new WebSocket(`ws://localhost:8080?token=${token}`);
+    addSocketHandlers(socket);
+}
+
+function addSocketHandlers(socket) {
+    socket.onopen = e => {
+        console.log('Connection opened');
+    };
+
+    socket.onmessage = e => {
+        try {
+            const data = JSON.parse(e.data);
+            handleMessage(data);
+        } catch (err) {
+            console.log(`invalid data: ${err}`);
+        }
+    };
+
+    socket.onerror = e => {
+        console.log(`Something went wrong there: ${e.data}`)
+    };
+
+    socket.onclose = e => {
+        var wasClean = e.wasClean;
+        console.log(`Close wasClean: ${wasClean}`);
+    }
+}
+
+function handleMessage(msg) {
+    if (!msg || !msg.type || !msg.data) throw new Error('Invalid data');
+
+    switch (msg.type) {
+        case 'INITIALIZE':
+            init( msg.data);
+            break;
+        case 'UPDATE_STATE':
+            updateState(msg.data);
+            break;
+        default:
+            throw new Error('this type of message not recognized');
+    }
+}
+
+function init({
+    user,
+    settings,
+    state
+}) {
+    game = new LifeGame(user, settings);
+    game.init();
+    game.send = send;
+    game.setState(state)
+}
+
+function updateState(state) {
+    game.setState(state);
+}
+
+function send(data) {
+    const dataObj = {
+        type: 'ADD_POINT',
+        data: data
+    };
+    socket.send(JSON.stringify(dataObj));
+}
